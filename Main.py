23c02f93c5ac062b1efaf5a012d98ed52fa89923
Pedrognd <pg186459@gmail.com>
2023-05-11 17:41:01 -0300

    Correções de bugs, no 'FictiousBank02.ui'

--- Main.py
+++ Main.py
@@ -2,15 +2,34 @@ from PyQt5 import uic,QtWidgets,QtCore
 import ImageAndIcons
 import os
 
-class Account():
+class Func():
     def __init__(self):
-        self.BalanceAccount = 200
+        self.BalanceAccount = 2
+        self.LenBalance = len(f'R$ {self.BalanceAccount:.2f}')
+
+    def BalanceFormat(self):
+        Balance = f'R$ {self.BalanceAccount:_.2f}'
+        Balance = Balance.replace('.',',').replace('_','.')
+        return Balance
+    
+    def WidthSizeChange(self):
+        if self.LenBalance <= 9:
+            NewWd = 75
+        elif self.LenBalance <= 12:
+            NewWd = 105
+        elif self.LenBalance <= 14:
+            NewWd = 135
+        else:
+            NewWd = 150
+        return NewWd
 
 def SegundaTela():
+    MainScreen.BalanceLabel.setText(Func().BalanceFormat())
+    NewWidthSize = Func().WidthSizeChange()
+    MainScreen.BalanceLabel.setMaximumWidth(NewWidthSize)
     MainScreen.setWindowFlag(QtCore.Qt.FramelessWindowHint)
     MainScreen.setAttribute(QtCore.Qt.WA_TranslucentBackground)
     MainScreen.ExitBtn.clicked.connect(MainScreen.close)
-    MainScreen.BalanceLabel.setText(balance)
     MainScreen.show()
     LoginScreen.close()
 
@@ -22,6 +41,5 @@ LoginScreen.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 LoginScreen.ExitBtn.clicked.connect(LoginScreen.close)
 LoginScreen.LoginBtn.clicked.connect(SegundaTela)
 
-balance = f'R$ {Account().BalanceAccount:.2f}'
 LoginScreen.show()
 app.exec()
