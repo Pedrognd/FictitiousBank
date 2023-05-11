9733b8f81373741d3668eebbc77ed1f5270fa931
Pedrognd <pedrogndmelo@gmail.com>
2023-05-11 11:57:11 -0300

    Add imagem da tela de investimento

--- Main.py
+++ Main.py
@@ -2,13 +2,17 @@ from PyQt5 import uic,QtWidgets,QtCore
 import ImageAndIcons
 import os
 
+class Account():
+    def __init__(self):
+        self.BalanceAccount = 200
+
 def SegundaTela():
     MainScreen.setWindowFlag(QtCore.Qt.FramelessWindowHint)
     MainScreen.setAttribute(QtCore.Qt.WA_TranslucentBackground)
     MainScreen.ExitBtn.clicked.connect(MainScreen.close)
+    MainScreen.BalanceLabel.setText(balance)
     MainScreen.show()
     LoginScreen.close()
-
 
 app = QtWidgets.QApplication([])
 LoginScreen=uic.loadUi('FictitiousBank01.ui')
@@ -18,5 +22,6 @@ LoginScreen.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 LoginScreen.ExitBtn.clicked.connect(LoginScreen.close)
 LoginScreen.LoginBtn.clicked.connect(SegundaTela)
 
+balance = f'R$ {Account().BalanceAccount:.2f}'
 LoginScreen.show()
 app.exec()
